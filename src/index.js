58bda8afa6f1c64a09926ebdce06893254f91c6b
anks666 <frank@anks.xx.kg>
2025-11-19 23:04:42 +0800

    Update routes for custom domain configurations

--- src/index.js
+++ src/index.js
@@ -7,14 +7,14 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  ["docker." + CUSTOM_DOMAIN]: dockerHub,
-  ["quay." + CUSTOM_DOMAIN]: "https://quay.io",
-  ["gcr." + CUSTOM_DOMAIN]: "https://gcr.io",
-  ["k8s-gcr." + CUSTOM_DOMAIN]: "https://k8s.gcr.io",
-  ["k8s." + CUSTOM_DOMAIN]: "https://registry.k8s.io",
-  ["ghcr." + CUSTOM_DOMAIN]: "https://ghcr.io",
-  ["cloudsmith." + CUSTOM_DOMAIN]: "https://docker.cloudsmith.io",
-  ["ecr." + CUSTOM_DOMAIN]: "https://public.ecr.aws",
+  "docker.libra.xx.kg": dockerHub,
+  "quay.libra.xx.kg": "https://quay.io",
+  "gcr.libra.xx.kg": "https://gcr.io",
+  "k8s-gcr.libra.xx.kg": "https://k8s.gcr.io",
+  "k8s.libra.xx.kg": "https://registry.k8s.io",
+  "ghcr.libra.xx.kg": "https://ghcr.io",
+  "cloudsmith.libra.xx.kg": "https://docker.cloudsmith.io",
+  "ecr.libra.xx.kg": "https://public.ecr.aws",
 
   // staging
   ["docker-staging." + CUSTOM_DOMAIN]: dockerHub,
